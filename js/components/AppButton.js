c28b91b8c92904170617c36cd5f2fbdc92bb9102
ariszak98 <ariszak98@icloud.com>
2026-04-22 21:56:23 +0300

    Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -2,15 +2,29 @@ export default {
 
 
         template:`
-            <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+            <button 
+                :class="{
+                    'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                    'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                    'bg-blue-500 hover:bg-blue-700 text-white': type === 'primary',
+                    'bg-purple-500 hover:bg-purple-700 text-white': type === 'secondary',
+                    'is-loading': processing
+                }" 
+                :disabled="processing">
                 <slot />
             </button>
         `,
 
-        data() {
-            return {
-                processing: true
+        props: {
+            type: {
+                type: String,
+                default: 'primary'
             }
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
         }
 
 
